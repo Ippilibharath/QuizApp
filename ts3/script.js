e826d6f5d9a39579a5f33bbc1539e9738a70b93c
Ippilibharath <108474893+Ippilibharath@users.noreply.github.com>
2023-07-10 23:47:38 +0530

    Add files via upload

---
+++ ts3/script.js
@@ -0,0 +1,93 @@
+const quizData = [
+    {
+        question: "Which language runs in a Web Browser?",
+        a: "C",
+        b: "python",
+        c: "java script",
+        d: "java",
+        correct: "c",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central style sheets",
+        b: "Cascading style sheets",
+        c: "Cascading simple sheets",
+        d: "Cars Suvs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "None of These",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1994",
+        b: "1998",
+        c: "1995",
+        d: "1996",
+        correct: "c",
+    },
+];
+
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => {
+        answerEl.checked = false;
+    });
+}
+
+function getSelected() {
+    let answer = undefined;
+    answerEls.forEach(answerEl => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
